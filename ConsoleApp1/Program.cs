4dd9e98f177c467490ca7ed0d1228e63f5be02ea
alex parfenov <alexp-2004@mail.ru>
2022-03-02 21:47:26 +0700

    Добавлены тесты + откорректированы файлы

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,11 +10,11 @@ namespace ConsoleApp1
             {
                 RomanNumber number1 = new RomanNumber(15); RomanNumber number2 = new RomanNumber(4); RomanNumber number3 = new RomanNumber(10);
                 RomanNumber number4 = new RomanNumber(50); RomanNumber number5 = new RomanNumber(89);
-                Console.WriteLine($"Vichitanie: {number5.ToString()} - {number4.ToString()} = {RomanNumber.Sub(number5, number4).ToString()} \n");
-                Console.WriteLine($"Umnojenie: {number3.ToString()} * {number4.ToString()} = {RomanNumber.Mul(number3, number4).ToString()} \n");
-                Console.WriteLine($"Slojenie: {number1.ToString()} + {number5.ToString()} = {RomanNumber.Add(number1, number5).ToString()} \n");
-                Console.WriteLine($"Delenie: {number1.ToString()} / {number2.ToString()} = {RomanNumber.Div(number1, number2).ToString()} \n");
-                
+                Console.WriteLine($"Сложение: {number1.ToString()} + {number5.ToString()} = {(number1 + number5).ToString()}");
+                Console.WriteLine($"Деление: {number1.ToString()} / {number2.ToString()} = {(number1 / number2).ToString()}");
+                Console.WriteLine($"Вычитание: {number5.ToString()} - {number4.ToString()} = {(number5 - number4).ToString()}");
+                Console.WriteLine($"Умножение: {number2.ToString()} * {number3.ToString()} = {(number2 * number3).ToString()}");
+
                 RomanNumber[] numbers = { number1, number2, number3, number4, number5 };
                 Array.Sort(numbers);
                 Console.WriteLine("Rezult sortirovki \n");
--- ConsoleApp1/RomanNumber.cs
+++ ConsoleApp1/RomanNumber.cs
@@ -6,94 +6,92 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-        internal class RomanNumber : ICloneable, IComparable
+    public class RomanNumber : ICloneable, IComparable
+    {
+
+        private ushort _number;
+        private static int[] values = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        private static string[] roman = new string[]
+            { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        public RomanNumber(ushort n)
+        {
+            if (n <= 0) throw new RomanNumberException($"Число {n} меньше либо равно 0");
+            else this._number = n;
+        }
+        public static RomanNumber operator +(RomanNumber? n1, RomanNumber? n2)
+        {
+            int num = n1._number + n2._number;
+            if (num <= 0) throw new RomanNumberException("Не удалось сложить  числа!");
+            else
+            {
+                return new RomanNumber((ushort)num);
+            }
+        }
+        public static RomanNumber operator -(RomanNumber? n1, RomanNumber? n2)
+        {
+            int num = n1._number - n2._number;
+            if (num <= 0) throw new RomanNumberException("Результат вычитания меньше либо равен 0!");
+            else
+            {
+                return new RomanNumber((ushort)num);
+            }
+        }
+        public static RomanNumber operator *(RomanNumber? n1, RomanNumber? n2)
+        {
+            int num = n1._number * n2._number;
+            if (num <= 0) throw new RomanNumberException("Не удалось умножить 2 числа!");
+            else
+            {
+                return new RomanNumber((ushort)num);
+            }
+        }
+
+        public static RomanNumber operator /(RomanNumber? n1, RomanNumber? n2)
         {
 
-            private ushort _number;
-            private static int[] values = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-            private static string[] roman = new string[]
-                { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-            public RomanNumber(ushort n)
+            if (n2._number == 0) throw new RomanNumberException("Ошибка деления!");
+            else
             {
-                if (n <= 0) throw new RomanNumberException($"Chislo {n} <= 0");
-                else this._number = n;
-            }
-            public static RomanNumber Add(RomanNumber? n1, RomanNumber? n2)
-            {
-                int num = n1._number + n2._number;
-                if (num <= 0) throw new RomanNumberException("Ne udalos` summirovat`  chisla");
+                int num = n1._number / n2._number;
+                if (num == 0) throw new RomanNumberException("Ошибка деления!");
                 else
                 {
-                    RomanNumber result = new((ushort)num);
-                    return result;
+                    return new RomanNumber((ushort)num);
                 }
             }
-            public static RomanNumber Sub(RomanNumber? n1, RomanNumber? n2)
+        }
+        public override string ToString()
+        {
+            int tmp = _number;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < 13; i++)
             {
-                int num = n1._number - n2._number;
-                if (num <= 0) throw new RomanNumberException("Rezultat vichitanya <= 0");
-                else
+                while (tmp >= values[i])
                 {
-                    RomanNumber result = new((ushort)num);
-                    return result;
+                    tmp -= (ushort)values[i];
+                    result.Append(roman[i]);
                 }
             }
-            public static RomanNumber Mul(RomanNumber? n1, RomanNumber? n2)
-            {
-                int num = n1._number * n2._number;
-                if (num <= 0) throw new RomanNumberException("Ne udalos` umnozhit` 2 chisla");
-                else
-                {
-                    RomanNumber result = new((ushort)num);
-                    return result;
-                }
-            }
-            public static RomanNumber Div(RomanNumber? n1, RomanNumber? n2)
-            {
-
-                if (n2._number == 0) throw new RomanNumberException("Error delenie");
-                else
-                {
-                    int num = n1._number / n2._number;
-                    if (num == 0) throw new RomanNumberException("Error delenie");
-                    else
-                    {
-                        RomanNumber result = new((ushort)num);
-                        return result;
-                    }
-                }
-            }
-            public override string ToString()
-            {
-                int tmp = _number;
-                StringBuilder result = new StringBuilder();
-                for (int i = 0; i < 13; i++)
-                {
-                    while (tmp >= values[i])
-                    {
-                        tmp -= (ushort)values[i];
-                        result.Append(roman[i]);
-                    }
-                }
-                if (result.ToString() == "")
-                    throw new RomanNumberException("Perevod nevozmojen");
-                else
-                    return result.ToString();
-
-            }
-
-            public object Clone()
-            {
-                return new RomanNumber(_number);
-            }
-
-            public int CompareTo(object obj)
-            {
-                if (obj is RomanNumber roman)
-                    return _number.CompareTo(roman._number);
-                else
-                    throw new RomanNumberException("object is not a RomanNumber");
-            }
+            if (result.ToString() == "")
+                throw new RomanNumberException("Перевод числа в Римские цифры невозможен");
+            else
+                return result.ToString();
 
         }
+
+        public object Clone()
+        {
+
+            return new RomanNumber(_number);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is RomanNumber roman)
+                return _number.CompareTo(roman._number);
+            else
+                throw new RomanNumberException("object is not a RomanNumber");
+        }
+
+    }
 }
